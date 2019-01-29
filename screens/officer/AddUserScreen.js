0e5f3c845c21601d4746f27acd255abfd772d757
Noppadon Sodram <knopsod@gmail.com>
2019-01-30 04:14:41 +0700

    picker of prefix, faculty, status are ok

--- screens/officer/AddUserScreen.js
+++ screens/officer/AddUserScreen.js
@@ -6,7 +6,11 @@ import {
   Button,
   Item,
   Input,
-  Form
+  Form,
+  Radio,
+  Picker,
+  Title,
+  Header
 } from "native-base";
 
 import firebase from '../../Firebase'
@@ -26,6 +30,31 @@ export default class AddUserScreen extends React.Component {
       status: navigation.getParam('status', ''),
       phoneNo: navigation.getParam('phoneNo', ''),
     };
+  }
+  onPressMr = () => {
+    this.setState({
+      prefix: 'mr',
+    })
+  }
+  onPressMs = () => {
+    this.setState({
+      prefix: 'ms',
+    })
+  }
+  onPressMrs = () => {
+    this.setState({
+      prefix: 'mrs',
+    })
+  }
+  onValueChange(value) {
+    this.setState({
+        faculty : value
+    })
+  }
+  onStatusChange(value) {
+    this.setState({
+        status : value
+    })
   }
   onPress = () => {
     const { 
@@ -52,21 +81,6 @@ export default class AddUserScreen extends React.Component {
       return;  
     }
 
-    // https://medium.com/mindorks/firebase-realtime-database-with-react-native-5f357c6ee13b
-    // firebase.database().ref('Users').push(this.state)
-    //   .then((data) => {
-    //     this.setState({
-    //       username: '',
-    //       password: '',
-    //       confirmPassword: '',
-    //       studentId: '',
-    //       firstName: '',
-    //       lastName: ''
-    //     });
-
-    //     this.props.navigation.goBack();
-    //   });
-
     // https://www.youtube.com/watch?v=BWIN4JBm0-k&list=PLy9JCsy2u97m-xWAxGwHZ2vITtj4qBKDm&index=6
     // https://github.com/nathvarun/React-Native-Firebase-Tutorials/blob/master/Project%20Files/4%265%20Swipeable%20Lists/Complete/App.js
     var key = this.state._key ? this.state._key : firebase.database().ref('Users').push().key;
@@ -87,6 +101,15 @@ export default class AddUserScreen extends React.Component {
     });
   }
   render() {
+    const { _key, 
+      studentId, 
+      idCardNo, 
+      prefix, 
+      firstName, 
+      lastName, 
+      faculty, 
+      status, 
+      phoneNo } = this.state;
     return (
       <Container style={styles.container}>
         <Content>
@@ -94,48 +117,80 @@ export default class AddUserScreen extends React.Component {
             <Item style={{ marginRight: 15 }}>
               <Input placeholder="รหัสนักศึกษา" name="studentId"
                 keyboardType="numeric"
-                value={this.state.studentId}
+                value={studentId}
                 onChangeText={val => this.setState({ studentId: val })} />
             </Item>
             <Item style={{ marginRight: 15 }}>
               <Input placeholder="เลขบัตรประชาชน" name="idCardNo"
                 keyboardType="numeric"
-                value={this.state.idCardNo}
+                value={idCardNo}
                 onChangeText={val => this.setState({ idCardNo: val })} />
             </Item>
-            <Item style={{ marginRight: 15 }}>
-              <Input placeholder="คำนำหน้าชื่อ" name="prefix"
-                value={this.state.prefix}
-                onChangeText={val => this.setState({ prefix: val })} />
+            <Item style={{ marginRight: 15, flexDirection: 'row', paddingTop: 10, paddingBottom: 10 }}>
+              <Radio selected={prefix === 'mr'}
+                onPress={() => this.onPressMr()} />
+              <Text> นาย     </Text>
+              <Radio selected={prefix === 'mrs'}
+                onPress={() => this.onPressMrs()} />
+              <Text> นาง     </Text>
+              <Radio selected={prefix === 'ms'}
+                onPress={() => this.onPressMs()} />
+              <Text> นางสาว</Text>
             </Item>
             <Item style={{ marginRight: 15 }}>
               <Input placeholder="ชื่อ" name="firstName"
-                value={this.state.firstName}
+                value={firstName}
                 onChangeText={val => this.setState({ firstName: val })} />
             </Item>
             <Item style={{ marginRight: 15 }}>
               <Input placeholder="นามสกุล" name="lastname"
-                value={this.state.lastName}
+                value={lastName}
                 onChangeText={val => this.setState({ lastName: val })} />
             </Item>
             <Item style={{ marginRight: 15 }}>
-              <Input placeholder="สาขาวิชา" name="faculty"
-                value={this.state.faculty}
-                onChangeText={val => this.setState({ faculty: val })} />
-            </Item><Item style={{ marginRight: 15 }}>
-              <Input placeholder="สถานะ" name="status"
-                value={this.state.status}
-                onChangeText={val => this.setState({ status: val })} />
-            </Item><Item style={{ marginRight: 15 }}>
+              <Picker
+                headerComponent={
+                  <Header>
+                    <Button transparent>
+                      Custom Back
+                    </Button>
+                    <Title>Custom Header</Title>
+                  </Header>
+                }
+                mode='dropdown'
+                selectedValue={faculty}
+                onValueChange={this.onValueChange.bind(this)}>
+                <Item label='ICT' value='ICT' />
+                <Item label='SC' value='SC' />
+              </Picker>
+            </Item>
+            <Item style={{ marginRight: 15 }}>
+            <Picker
+                headerComponent={
+                  <Header>
+                    <Button transparent>
+                      Custom Back
+                    </Button>
+                    <Title>Custom Header</Title>
+                  </Header>
+                }
+                mode='dropdown'
+                selectedValue={status}
+                onValueChange={this.onStatusChange.bind(this)}>
+                <Item label='นักศึกษาทั่วไป' value='s' />
+                <Item label='สโมสร' value='c' />
+              </Picker>
+            </Item>
+            <Item style={{ marginRight: 15 }}>
               <Input placeholder="เบอร์โทรศัพท์" name="phoneNo"
                 keyboardType="phone-pad"
-                value={this.state.phoneNo}
+                value={phoneNo}
                 onChangeText={val => this.setState({ phoneNo: val })} />
             </Item>
           </Form>
           <Button block style={{ margin: 5, marginTop: 20 }}
             onPress={() => this.onPress()}>
-            <Text style={{ color: '#fff' }}>{ this.state._key ? 'บันทึก' : 'สร้างใหม่'}</Text>
+            <Text style={{ color: '#fff' }}>{ _key ? 'บันทึก' : 'สร้างใหม่'}</Text>
           </Button>
         </Content>
       </Container>
