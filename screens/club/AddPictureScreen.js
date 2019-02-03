847099eff52bbfcba3b52116eee6d5f0ee6f22e9
Noppadon Sodram <knopsod@gmail.com>
2019-02-04 02:50:59 +0700

    upload success

--- screens/club/AddPictureScreen.js
+++ screens/club/AddPictureScreen.js
@@ -13,105 +13,72 @@ import {
   Right,
   Icon
 } from "native-base";
-
-import firebase from '../../Firebase'
+import firebase from '../../Firebase';
+import { ImagePicker, Permissions } from 'expo';
+import uuid from 'uuid';
 
 export default class AddPictureScreen extends React.Component {
   constructor(props) {
     super(props);
+
     const { navigation } = props;
     this.state = {
-      _key: '',
       activityKey: navigation.getParam('_key', ''),
-      blob: '',
-      pictures: [],
     };
-
-    this.child = firebase.database().ref().child('Pictures').orderByChild('activityKey').equalTo(navigation.getParam('_key'));
-    this.ref = firebase.database().ref().child('Pictures');
   }
-  componentDidMount() {
-    // https://www.youtube.com/watch?v=Di607bTqhPc&t=2186s
-    // https://github.com/rayn-studios-learning/message-board-app/blob/master/App.js
-    this.child.on('child_added', snapshot => {
-      const data = snapshot.val();
-      if (data) {
-        this.setState(prevState => ({
-          pictures: [data, ...prevState.pictures]
-        }))
-      }
-    });
 
-    this.child.on('child_removed', snapshot => {
-      const { _key } = snapshot.val();
+  // https://www.youtube.com/watch?v=KkZckepfm2Q
+  onChooseImagePress = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync();
 
-      this.setState({
-        pictures: this.state.pictures.filter(element => element._key !== _key)
-      });
-    })
-  }
-  onPress = () => {
-    const { 
-      activityKey,
-      blob
-    } = this.state;
-
-    // https://www.youtube.com/watch?v=BWIN4JBm0-k&list=PLy9JCsy2u97m-xWAxGwHZ2vITtj4qBKDm&index=6
-    // https://github.com/nathvarun/React-Native-Firebase-Tutorials/blob/master/Project%20Files/4%265%20Swipeable%20Lists/Complete/App.js
-    var key = this.state._key ? this.state._key : firebase.database().ref('Pictures').push().key;
-    var set = firebase.database().ref('Pictures').child(key).set({ activityKey, blob, _key: key });
-    set.then((data) => {
-        this.setState({
-          blob: ''
+    if (!result.cancelled) {
+      console.log('!cancelled');
+      this.uploadImage(result.uri)
+        .then(() => {
+          Alert.alert('อัพโหลด', 'สำเร็จ')
+        })
+        .catch((error) => {
+          Alert.alert('อัพโหลด', 'ล้มเหลว')
         });
+    }
+  }
 
-        // this.props.navigation.goBack();
+  uploadImage = async (uri) => {
+    const { activityKey } = this.state;
+    
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function(e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', uri, true);
+      xhr.send(null);
     });
+
+    var ref = firebase.storage().ref(activityKey).child(uuid.v4());
+    const snapshot = await ref.put(blob);
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
   }
-  confirmRemove(data) {
-    const { _key, blob } = data;
-    Alert.alert(
-      'ลบ',
-      `${blob}`,
-      [
-        {
-          text: 'ยกเลิก',
-          style: 'cancel'
-        },
-        {
-          text: 'ลบ',
-          onPress: () => {
-            this.ref.child(_key).remove();
-          }
-        }
-      ],
-      { cancelable: false }
-    )
-  }
+  
   render() {
-    const { pictures } = this.state;
+
     return (
       <Container style={styles.container}>
         <Content>
           <Button block style={{ margin: 5, marginTop: 20 }}
-            onPress={() => this.onPress()}>
+            onPress={this.onChooseImagePress}>
             <Text style={{ color: '#fff' }}>อัพโหลด</Text>
           </Button>
-          <List
-            dataArray={pictures}
-            renderRow={data => {
-              return <ListItem
-                button
-                onPress={() => this.confirmRemove(data)}
-              >
-                <Left>
-                  <Text>
-                    {data.blob}
-                  </Text>
-                </Left>
-              </ListItem>}
-            }
-          />
+          
         </Content>
       </Container>
     );
